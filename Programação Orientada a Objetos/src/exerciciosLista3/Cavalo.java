f3d6e0a6e8f3ba6876315094c00f9bbd489161f6
Felipe Casagrande <77506504+fehcasa@users.noreply.github.com>
2021-02-26 15:03:05 -0300

    Exercícios de programação orientada a objetos

---
+++ Programação Orientada a Objetos/src/exerciciosLista3/Cavalo.java
@@ -0,0 +1,25 @@
+package exerciciosLista3;
+
+public class Cavalo extends Animal
+{
+	
+	public Cavalo ()
+	{
+		
+	}
+	
+	public void som()
+	{
+		System.out.println("pooc potoc pooc potoc");
+	}
+	public void corre()
+	{
+		System.out.println("Cavalo marchando...\n");
+	}
+	
+	public String mostraCavalo()
+	{
+		return "Nome do cavalo: " + nome + "\nIdade do cavalo: " + idade;
+	}
+
+}
